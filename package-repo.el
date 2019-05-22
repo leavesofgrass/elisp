2b74c8bfb078b89453b4c47fea88c8fb7009c8dc
Jon Pielaet <jon@pielaet.net>
2019-05-22 12:03:34 -0700

    refactor

--- package-repo.el
+++ package-repo.el
@@ -20,30 +20,26 @@
 
 ;; Packages
 
-(require 'cl)
 (require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-
-  (when (< emacs-major-version 24)
-
-    ;; For important compatibility libraries like cl-lib
-
-    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
-
-;; org
-
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
 
 (package-initialize)
 
-;; Paradox default interface
+;; init-packages
+
+(unless (package-installed-p 'async)
+  (package-refresh-contents)
+  (package-install 'async))
+
+(unless (package-installed-p 'paradox)
+  (package-refresh-contents)
+  (package-install 'paradox))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 (paradox-enable)
