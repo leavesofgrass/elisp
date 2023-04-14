cee36088c4b06220f4aab90b280e9ebbf5e509eb
Jon Pielaet <jon@pielaet.net>
2023-04-13 19:00:44 -0700

    linum for undo-tree not needed any longer

--- func.el
+++ func.el
@@ -23,24 +23,6 @@
 ;; avoid duplication of the paradox buffer if one already exists
 ;; otherwise create one when called
 
-;; undo-tree really hates linum
-
-(defun undo-tree-visualizer-update-linum
-    (&rest
-     args)
-  (linum-update undo-tree-visualizer-parent-buffer))
-(advice-add 'undo-tree-visualize-undo
-            :after #'undo-tree-visualizer-update-linum)
-(advice-add 'undo-tree-visualize-redo
-            :after #'undo-tree-visualizer-update-linum)
-(advice-add 'undo-tree-visualize-undo-to-x
-            :after #'undo-tree-visualizer-update-linum)
-(advice-add 'undo-tree-visualize-redo-to-x
-            :after #'undo-tree-visualizer-update-linum)
-(advice-add 'undo-tree-visualizer-mouse-set
-            :after #'undo-tree-visualizer-update-linum)
-(advice-add 'undo-tree-visualizer-set
-            :after #'undo-tree-visualizer-update-linum)
 ;;paradox
 
 (defun visit-paradox ()
